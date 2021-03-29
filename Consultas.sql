5953bf2ea0c8598732ebbebb5ffbe70882a318b7
Santiago Rivadeneira <2666910480101@ingenieria.usac.edu.gt>
2021-03-28 20:27:44 -0600

    se agregaron endpoints para cargar temporal, eliminar temporal y eliminar modelo

---
+++ Consultas.sql
@@ -0,0 +1,37 @@
+USE GVE;
+
+/*CONSULTA 1*/
+/*
+SELECT Hospital.nombre_hospital AS nombre, Ubicacion_Hospital.direccion AS direccion , COUNT(RegistroVictima.fecha_hora_fallecimiento) as fallecidos
+FROM Hospital
+INNER JOIN Ubicacion_Hospital ON Hospital.idUbicacion_Hospital = Ubicacion_Hospital.idUbicacion_Hospital
+INNER JOIN RegistroVictima  ON Hospital.idRegistroVictima = RegistroVictima.idRegistroVictima
+GROUP BY nombre,direccion;
+*/
+
+/*CONSULTA 2*/
+/*
+SELECT Nombre, Apellido
+FROM(
+SELECT Tratamiento.idVictima AS idVictima , Tratamiento.efectividad_paciente AS efePaciente, Tipo_Tratamiento.descripcion_tipo AS tratamiento, Victima.nombre AS Nombre , Victima.apellido AS Apellido 
+FROM Tratamiento
+INNER JOIN Tipo_Tratamiento ON Tratamiento.idTipo_Tratamiento = Tipo_Tratamiento.idTipo_Tratamiento
+INNER JOIN Victima ON Victima.idVictima = Tratamiento.idVictima
+)informacionVictima WHERE tratamiento LIKE '%Transfusiones de sangre%%' AND efePaciente > 5;
+*/
+
+/*CONSULTA 3*/
+/*
+SELECT RegistroVictima.fecha_hora_fallecimiento AS muerto ,Victima.idVictima AS idV ,Victima.nombre AS Nombre,Victima.apellido AS Apellido,Contacto.idVictima as idConteo, COUNT(Contacto.idVictima) as conteo
+FROM RegistroVictima
+INNER JOIN Victima ON RegistroVictima.idVictima = Victima.idVictima AND RegistroVictima.fecha_hora_fallecimiento != NULL
+INNER JOIN Contacto ON RegistroVictima.idVictima = Contacto.idVictima AND conteo > 3
+GROUP BY Nombre,Apellido;
+*/
+/*
+SELECT 
+FROM Contacto
+GROUP BY Contacto.idVictima;
+*/
+SELECT * FROM Temporal;
+/*DROP TABLE IF EXISTS Temporal;*/
--- src/routes/consultas.js
+++ src/routes/consultas.js
@@ -7,6 +7,15 @@ api.get("/",(req,res)=>{
                 "Registro academico":"201313722"});
 });
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+   host: 'localhost',
+   user: 'root',
+   password: '130414',
+   database: 'GVE',
+   port: 3306
+});
+
 //CONSULTA 1
 api.get("/consulta1",(req,res)=>{
     res.json({"Title":"Consulta 1"});
@@ -59,17 +68,35 @@ api.get("/consulta10",(req,res)=>{
 
 //ELIMINAR TEMPORAL
 api.get("/eliminarTemporal",(req,res)=>{
-    res.json({"Title":"Eliminar Temporal"});
+    var eval = eliminarTemporal();
+    if(eval == 1){
+        res.json({"Title":"Tabla Temporal Eliminada"});
+    }else{
+        res.json({"Title":"Problema Al Eliminar Temporal"});
+    }
+
 });
 
 //ELIMINAR MODELO
 api.get("/eliminarModelo",(req,res)=>{
-    res.json({"Title":"Eliminar Modelo"});
+    var eval = eliminarModelo();
+    if(eval == 1){
+        res.json({"Title":"Modelo Eliminado"});
+    }else{
+        res.json({"Title":"Problema Al Eliminar DB"});
+    }
 });
 
 //CARGA TEMPORAL
-api.get("/cargaTemporal",(req,res)=>{
-    res.json({"Title":"Carga Temporal"});
+api.post("/cargarTemporal",(req,res)=>{
+    var t = crearTemporal(req.body.Ruta);
+    if(t == 1){
+        res.json({"Title":"Creacion y carga de tabla Temporal Exitosa!"});
+    }else if (t == 0){
+        res.json({"Title":"Problema al crear tabla Temporal"});
+    }else{
+        res.json({"Title":"Problema al cargar el archivo"});
+    }
 });
 
 //CARGAR MODELO
@@ -77,5 +104,97 @@ api.get("/cargarModelo",(req,res)=>{
     res.json({"Title":"Cargar Modelo"});
 });
 
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+///// FUNCIONES PARA  LAS CONSULTAS
+
+//cargar tabla temporal
+function crearTemporal(ruta){
+    var errCrear = 1;
+    var crearT = "CREATE TABLE Temporal(\
+        idTemporal INT NOT NULL AUTO_INCREMENT PRIMARY KEY,\
+        NOMBRE_VICTIMA VARCHAR (50) NOT NULL,\
+        APELLIDO_VICTIMA VARCHAR (50) NOT NULL,\
+        DIRECCION_VICTIMA VARCHAR (70) NOT NULL,\
+        FECHA_PRIMERA_SOSPECHA DATETIME NOT NULL,\
+        FECHA_CONFIRMACION DATETIME NOT NULL,\
+        FECHA_MUERTE DATETIME NULL,\
+        ESTADO_VICTIMA VARCHAR (40) NOT NULL,\
+        NOMBRE_ASOCIADO VARCHAR (50) NULL,\
+        APELLIDO_ASOCIADO VARCHAR (50) NULL,\
+        FECHA_CONOCIO DATETIME NULL,\
+        CONTACTO_FISICO VARCHAR (50) NULL,\
+        FECHA_INICIO_CONTACTO DATETIME NULL,\
+        FECHA_FIN_CONTACTO DATETIME NULL,\
+        NOMBRE_HOSPITAL VARCHAR(50) NULL,\
+        DIRECCION_HOSPITAL VARCHAR(70) NULL,\
+        UBICACION_VICTIMA VARCHAR (50) NULL,\
+        FECHA_LLEGADA DATETIME NULL,\
+        FECHA_RETIRO DATETIME NULL,\
+        TRATAMIENTO VARCHAR (50) NULL,\
+        EFECTIVIDAD INT NULL,\
+        FECHA_INICIO_TRATAMIENTO DATETIME NULL,\
+        FECHA_FIN_TRATAMIENTO DATETIME NULL,\
+        EFECTIVIDAD_EN_VICTIMA INT NULL\
+    );";
+    //console.log(ruta);
+    //console.log(crearT);
+    
+    connection.query(crearT, function (err, result) {
+        if (err) errCrear = 0;
+        console.log("Tabla Temporal Creada");
+    });
+    //si hubo un problema al crearse la tabla temporal
+    if(errCrear == 0){
+        return errCrear;
+    }
+    var cargarDatos ="LOAD  DATA LOCAL INFILE \'"+ ruta +"\'\
+    into table Temporal\
+    character set utf8mb4\
+    fields terminated by ';'\
+    ignore 1 lines\
+    (NOMBRE_VICTIMA,APELLIDO_VICTIMA,DIRECCION_VICTIMA,@FECHA_PRIMERA_SOSPECHA,@FECHA_CONFIRMACION,@FECHA_MUERTE,ESTADO_VICTIMA,NOMBRE_ASOCIADO,APELLIDO_ASOCIADO,@FECHA_CONOCIO,CONTACTO_FISICO,@FECHA_INICIO_CONTACTO,@FECHA_FIN_CONTACTO,NOMBRE_HOSPITAL,DIRECCION_HOSPITAL,UBICACION_VICTIMA,@FECHA_LLEGADA,@FECHA_RETIRO,TRATAMIENTO,EFECTIVIDAD,@FECHA_INICIO_TRATAMIENTO,@FECHA_FIN_TRATAMIENTO,EFECTIVIDAD_EN_VICTIMA)\
+    set FECHA_PRIMERA_SOSPECHA = STR_TO_DATE (@FECHA_PRIMERA_SOSPECHA, '%Y-%m-%d %H:%i:%s'),\
+    FECHA_CONFIRMACION = STR_TO_DATE (@FECHA_CONFIRMACION, '%Y-%m-%d %H:%i:%s'),\
+    FECHA_MUERTE = STR_TO_DATE (@FECHA_MUERTE, '%Y-%m-%d %H:%i:%s'),\
+    FECHA_CONOCIO = STR_TO_DATE (@FECHA_CONOCIO, '%Y-%m-%d %H:%i:%s'),\
+    FECHA_INICIO_CONTACTO = STR_TO_DATE (@FECHA_INICIO_CONTACTO, '%Y-%m-%d %H:%i:%s'),\
+    FECHA_FIN_CONTACTO = STR_TO_DATE (@FECHA_FIN_CONTACTO, '%Y-%m-%d %H:%i:%s'),\
+    FECHA_LLEGADA = STR_TO_DATE (@FECHA_LLEGADA, '%Y-%m-%d %H:%i:%s'),\
+    FECHA_RETIRO = STR_TO_DATE (@FECHA_RETIRO, '%Y-%m-%d %H:%i:%s'),\
+    FECHA_INICIO_TRATAMIENTO = STR_TO_DATE (@FECHA_INICIO_TRATAMIENTO, '%Y-%m-%d %H:%i:%s'),\
+    FECHA_FIN_TRATAMIENTO = STR_TO_DATE (@FECHA_FIN_TRATAMIENTO, '%Y-%m-%d %H:%i:%s');\
+    ";
+
+    connection.query(cargarDatos, function (err, result) {
+        if (err) errCrear = 2;
+        console.log("Tabla Temporal Cargada");
+    });
+    
+    return errCrear;
+    
+}
+
+//eliminar temporal
+function eliminarTemporal(){
+    var eliminarT = "DROP TABLE IF EXISTS Temporal;";
+    var t = 1;
+    connection.query(eliminarT, function (err, result) {
+        if(err) t =  0
+    });
+    return t;
+}
+
+//eliminar base de datos
+function eliminarModelo(){
+    var eliminarT = "DROP DATABASE IF EXISTS GVE;";
+    var t = 1;
+    connection.query(eliminarT, function (err, result) {
+        if(err) t =  0
+    });
+    return t;
+}
 
 module.exports = api;
